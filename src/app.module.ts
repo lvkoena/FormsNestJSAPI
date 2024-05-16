7ec8aae7176df67ba2559fab0299109d2f096994
LeboHIndsafri <Lebohang.mokoena@indsafri.com>
2024-05-16 15:57:54 +0200

    created CORS Policy

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,6 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { FormController } from './form/controllers/form/form.controller';
-import { FormService } from './form/services/form/form.service';
 import { FormModule } from './form/modules/form.module';
+
 
 
 @Module({
@@ -11,4 +8,5 @@ import { FormModule } from './form/modules/form.module';
   controllers: [],
   providers: [],
 })
+
 export class AppModule {}
--- src/form/dto/create-form.dto.ts
+++ src/form/dto/create-form.dto.ts
@@ -1,7 +1,6 @@
 export class CreateFormDto {
-    readonly firstName: string;
-    readonly lastName: string;
-    readonly emailAddress: string;
-    readonly phonePhone: number;
-    readonly message: string
+    firstName: string;
+    lastName: string;
+    emailAddress: string;
+    message: string
 }
